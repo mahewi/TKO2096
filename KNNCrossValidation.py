fd02474035e486e74f1c6df5eb30d605cc64737a
Marco Willgren <mhw1@hotmail.com>
2015-01-17 19:41:55 +0200

    Initial commit with base project

---
+++ KNNCrossValidation.py
@@ -0,0 +1,18 @@
+'''
+Exercise 1: Application of data analysis
+
+K-nearest neighbors and cross-validation
+- Program your own implementation of kNN. Preferably with python.
+- Select a data set of your chouse from the UCI machine learning repository: \url{http://archive.ics.uci.edu/ml/}
+- Use cross-validation on the training set and report the performance (e.g. classification or regression error) for different values of $k$
+
+Phases: 
+1. Parse data: Divide data into test and train sets.
+2. Similarity: Calculate the distances data instances.
+3. Neighbors: Locate k most similar data instances.
+4. Response: Generate a response from a set of data instances.
+5. Accuracy: Summarize the accuracy of predictions.
+'''
+
+if __name__ == '__main__':
+    pass
