e82d099f22a8c7d0be3a0e5214f99b0442a933eb
Marco Willgren <mhw1@hotmail.com>
2015-01-19 11:24:37 +0200

    Added comments

--- KNNCrossValidation.py
+++ KNNCrossValidation.py
@@ -11,16 +11,17 @@ K-nearest neighbors and cross-validation
 - Use cross-validation on the training set and report the performance (e.g. classification or regression error) for different values of $k$
 
 Phases: 
-1. Parse data: Divide data into test and train sets.
-2. Similarity: Calculate the distances data instances.
-3. Neighbors: Locate k most similar data instances.
-4. Response: Generate a response from a set of data instances.
-5. Accuracy: Summarize the accuracy of predictions.
+1. Divide data into test and train sets.
+2. Calculate the distances data instances.
+3. Locate k most similar data instances.
+4. Generate a response from a set of data instances.
+5. Summarize the accuracy of the predictions.
 
 '''
 
 
 import os
+import random
 import operator
 import numpy as np
 import scipy.spatial.distance as ssd
@@ -28,13 +29,13 @@ import scipy.spatial.distance as ssd
 if __name__ == '__main__':
     pass
 
-# Set pointer to correct destination. fertility_diag.txt contains: 
+# fertility_diag.txt contains: 
 #     - 100 volunteers provide a semen sample analyzed according to the WHO 2010 criteria. 
 #       Sperm concentration are related to socio-demographic data, environmental factors, health status, and life habits.
 #     - Number of instances : 100
 #     - Number of attributes : 10
 
-
+# Set pointer to correct destination to access the data file. 
 basepath = os.path.dirname(__file__)
 filepath = os.path.abspath(os.path.join(basepath, "fertility_diag.txt"))
 
@@ -42,16 +43,19 @@ filepath = os.path.abspath(os.path.join(basepath, "fertility_diag.txt"))
 x = np.loadtxt(filepath, delimiter=',', usecols=range(0,9))
 y = np.genfromtxt(filepath, dtype='str', delimiter=',', usecols=(9))
 
+
 # Split the data into four sets: 
-#    - Train/test sets for feature data and train/test sets for outcome labels.
-def splitToTrainAndTest():
+#    - Train/test sets for feature data and train/test sets for outcome labels (O/N). 
+#    - Method argument == posibility to be included into test set
+#    - Returns train and test sets
+def splitToTrainAndTest(split):
     xTrain = []
     yTrain = []
     xTest = []
     yTest = []
     
     for i in range(len(x) - 1):
-        if (i < (len(x)*0.50)):
+        if (random.random() < split):
             yTest.append(y[i])
             xTest.append(x[i])
         else:
@@ -61,7 +65,8 @@ def splitToTrainAndTest():
     return xTrain, yTrain, xTest, yTest
 
 
-# Calculate eucledian distance against a single test instance against each train instance.
+# Calculate eucledian distances for a single test instance against each train instance.
+# Return list of distances with label in increasing order
 def calculateDistances(trainSet, testInstance, trainLabel):
     distances = []
     
@@ -73,19 +78,22 @@ def calculateDistances(trainSet, testInstance, trainLabel):
     return distances
 
 
-def inferNeighbours(distances, k):
-    neighbours = []
+# Returns k nearest neighbors (sorted list)
+def inferNeighbors(distances, k):
+    neighbors = []
     for x in range(k):
-        neighbours.append(distances[x])
+        neighbors.append(distances[x])
         
-    return neighbours
+    return neighbors
         
 
-def chooseMajorityLabel(neighbours):
+# Calculates labels of the neighbors
+# Returns the most popular neighbor
+def chooseMajorityLabel(neighbors):
     labelCounts = {}
     
-    for x in range(len(neighbours)):
-        label = neighbours[x][1]
+    for x in range(len(neighbors)):
+        label = neighbors[x][1]
         if label in labelCounts:
             labelCounts[label] += 1
         else:
@@ -93,6 +101,8 @@ def chooseMajorityLabel(neighbours):
     return sorted(labelCounts.iteritems(), key=operator.itemgetter(1), reverse=True)[0][0]
 
 
+# Compares the predicted labels to corresponding labels in the test set 
+# Returns the error percentage of the comparison
 def calculateErrorPercentage(predictedLabels, yTest):
     errorCount = 0.0
     for x in range(len(predictedLabels)):
@@ -102,26 +112,30 @@ def calculateErrorPercentage(predictedLabels, yTest):
     return errorCount/len(yTest)*100.0
             
 
+# Print the resulting accuracy of the prediction with value K
 def printOutcome(errorPercentage, k):
     print 'The accuracy of the predition with value k={k}: {errorPercentage} %'.format(k=k, errorPercentage=errorPercentage)
 
     
 # Run method
-def mainMethod(k):
-    xTrain, yTrain, xTest, yTest = splitToTrainAndTest()
-    eucDistances = []
-    neighbours = []
-    predictedLabels = []
+# Calculate KNN with different values of K.
+def mainMethod():
+    xTrain, yTrain, xTest, yTest = splitToTrainAndTest(0.40)
+    k = 31
     
-    for y in range(len(xTest)):
-        eucDistances = calculateDistances(xTrain, xTest[y], yTrain)
-        neighbours = inferNeighbours(eucDistances, k)
-        predictedLabels.append(chooseMajorityLabel(neighbours))
+    for i in range(1, k):
+        eucDistances = []
+        neighbors = []
+        predictedLabels = []
+        
+        for y in range(len(xTest)):
+            eucDistances = calculateDistances(xTrain, xTest[y], yTrain)
+            neighbors = inferNeighbors(eucDistances, i)
+            predictedLabels.append(chooseMajorityLabel(neighbors))
+        
+        errorPercentage = calculateErrorPercentage(predictedLabels, yTest)
+        
+        printOutcome(errorPercentage, i)
     
-    errorPercentage = calculateErrorPercentage(predictedLabels, yTest)
-    
-    printOutcome(errorPercentage, k)
-    
-    
-for i in range(1,31):    
-    mainMethod(i)   
+      
+mainMethod()   
