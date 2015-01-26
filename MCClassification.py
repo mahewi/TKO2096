2bdff9a7269ec7d872642a7ede81f9186a9c5534
Marco Willgren <mhw1@hotmail.com>
2015-01-26 17:53:47 +0200

    Filter data set from 5600 -> 980 instances

--- MCClassification.py
+++ MCClassification.py
@@ -17,12 +17,12 @@ R2L = 4
 U2R = 5
 
 A training dataset of 5600 different labeled (Last column) network connection data instances for constructing the classifier.
-A test data set of 3100 datapoint (different distribution from training data) is given for testing the classifier, data is labeled   (Last column). 
+A testX data set of 3100 datapoint (different distribution from training data) is given for testing the classifier, data is labeled   (Last column). 
 
 Task:
 Use the K-nearest neighbor approach for the classification problem with different number of neighbors (k = 3,..., 10)
     * Cross validation on the training set (10-fold)
-    * Test the classifier's accuracy on the test data set and report the accuracy and F-score for the best number of neighbors (K)
+    * Test the classifier's accuracy on the testX data set and report the accuracy and F-score for the best number of neighbors (K)
     * Include the confusion matrix in your report
 
 '''
@@ -32,6 +32,7 @@ import numpy as np
 import scipy.spatial.distance as ssd
 import operator
 from matplotlib import pyplot as pp
+from numpy  import array
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score as fscore
 from sklearn.neighbors import KNeighborsClassifier
@@ -48,22 +49,64 @@ basepath = os.path.dirname(__file__)
 trainpath = os.path.abspath(os.path.join(basepath, "KDDcup99/train.csv"))
 testpath = os.path.abspath(os.path.join(basepath, "KDDcup99/test.csv"))
 
-# Parse train data into two variables trainX and trainY. x = feature data, y = outcome
-trainX = np.genfromtxt(trainpath, delimiter=',', usecols=range(0, 41))
-trainY = np.genfromtxt(trainpath, dtype=None, delimiter=',', usecols=(41))
+# Parse trainX data into two variables trainX and trainY. x = feature data, y = outcome
+tempTrainX = np.genfromtxt(trainpath, delimiter=',', usecols=range(0, 41))
+tempTrainY = np.genfromtxt(trainpath, dtype=None, delimiter=',', usecols=(41))
 
-# Parse test data into two variables testX and testY. x = feature data, y = outcome
-testX = np.genfromtxt(testpath, delimiter=',', usecols=range(0, 41))
-testY = np.genfromtxt(testpath, dtype=None, delimiter=',', usecols=(41))
+# Parse testX data into two variables testX and testY. x = feature data, y = outcome
+tempTestX = np.genfromtxt(testpath, delimiter=',', usecols=range(0, 41))
+tempTestY = np.genfromtxt(testpath, dtype=None, delimiter=',', usecols=(41))
 
-# Limit how many rows are handled
-dataLimit = 3000
+# Original data set too large -> filter 980 instances from each data set 
+trainX = []
+trainY = []
+testX = []
+testY = []
+for i in range(len(tempTrainX)):
+    if i >= 0 and i < 350:
+        trainX.append(tempTrainX[i])
+        trainY.append(tempTrainY[i])
+    if i >= 2000 and i < 2350:
+        trainX.append(tempTrainX[i])
+        trainY.append(tempTrainY[i])
+    if i >= 4000 and i < 4150:
+        trainX.append(tempTrainX[i])
+        trainY.append(tempTrainY[i])
+    if i >= 5000 and i < 5100:
+        trainX.append(tempTrainX[i])
+        trainY.append(tempTrainY[i])
+    if i >= 5500 and i < 5530:
+        trainX.append(tempTrainX[i])
+        trainY.append(tempTrainY[i])
+
+for i in range(len(tempTestX)):
+    if i >= 0 and i < 350:
+        testX.append(tempTestX[i])
+        testY.append(tempTestY[i])
+    if i >= 1000 and i < 1350:
+        testX.append(tempTestX[i])
+        testY.append(tempTestY[i])
+    if i >= 2000 and i < 2150:
+        testX.append(tempTestX[i])
+        testY.append(tempTestY[i])
+    if i >= 2500 and i < 2600:
+        testX.append(tempTestX[i])
+        testY.append(tempTestY[i])
+    if i >= 3000 and i < 3030:
+        testX.append(tempTestX[i])
+        testY.append(tempTestY[i])
+
+# Convert lists to numpy arrays
+trainX = array(trainX)
+trainY = array(trainY)
+testX = array(testX)
+testY = array(testY)
 
 
 # Main method used as a running class. Preprocess and normalize the data
 def main():
-    preProcessedTrainX = preProcessData(trainX[:dataLimit],trainpath)
-    preProcessedTestX = preProcessData(testX[:dataLimit],testpath)
+    preProcessedTrainX = preProcessData(trainX, trainpath)
+    preProcessedTestX = preProcessData(testX, testpath)
     normalizedTrainX = normalize(preProcessedTrainX)
     normalizedTestX = normalize(preProcessedTestX)
     
@@ -76,20 +119,20 @@ def main():
     
 # Plots confusion matrix
 def plotConfusionMatrix(predLabels):
-    cm = confusion_matrix(testY[:dataLimit], predLabels)
+    cm = confusion_matrix(testY, predLabels)
     pp.matshow(cm)
     pp.show()
 
 # Calculates and prints out accuracy of predicted labels and F-score
 def calculateAccuracyAndScore(predLabels):
-    print 'Accuracy: ' + str(100 - calculateErrorPercentage(predLabels, testY[:dataLimit]))
-    print 'F-score: ' + str(fscore(testY[:dataLimit],predLabels))
+    print 'Accuracy: ' + str(100 - calculateErrorPercentage(predLabels, testY))
+    print 'F-score: ' + str(fscore(testY, predLabels))
     
 # Predicts labels based on K neighbors. Uses sklear.neigbours.KNeighborsClassifier to predict the labels
 # Return labels
 def predictLabelsBasedOnKNeighbours(K,normTrainX,normTestX):
     knc = KNeighborsClassifier(n_neighbors=K)
-    knc.fit(normTrainX, trainY[:dataLimit])
+    knc.fit(normTrainX, trainY)
     predictedLabels = knc.predict(normTestX)
     return predictedLabels
 
@@ -114,10 +157,10 @@ def selectBestK(xTrain,xTest,yTrain,yTest):
     return K
     
 
-# Use stratified ten fold to create train and test sets
-# Return train and test sets
+# Use stratified ten fold to create trainX and testX sets
+# Return trainX and testX sets
 def crossValidate(normX):
-    skf = StratifiedKFold(trainY[:dataLimit], n_folds=10, shuffle=True)
+    skf = StratifiedKFold(trainY, n_folds=10, shuffle=True)
     for train_index, test_index in skf:
         X_train, X_test = normX[train_index], normX[test_index]
         y_train, y_test = trainY[train_index], trainY[test_index]
@@ -167,7 +210,7 @@ def preProcessData(x,path):
     return x
 
 
-# Calculate eucledian distances for a single test instance against each train instance.
+# Calculate eucledian distances for a single testX instance against each trainX instance.
 # Return list of distances with label in increasing order
 def calculateDistances(trainSet, testInstance, trainLabel):
     distances = []
@@ -202,7 +245,7 @@ def chooseMajorityLabel(neighbors):
     return sorted(labelCounts.iteritems(), key=operator.itemgetter(1), reverse=True)[0][0]
 
 
-# Compares the predicted labels to corresponding labels in the test set 
+# Compares the predicted labels to corresponding labels in the testX set 
 # Returns the error percentage of the comparison
 def calculateErrorPercentage(predictedLabels, yTest):
     errorCount = 0.0
