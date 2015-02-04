6f5fd5c8c8bef47b2f1ac4ab08ca7d8fbde2e52f
Marco Willgren <mhw1@hotmail.com>
2015-02-04 10:40:18 +0200

    Methods from previous assignment

--- DeadZoneCV.py
+++ DeadZoneCV.py
@@ -19,6 +19,8 @@ Task steps:
 '''
 
 import os
+import operator
+import scipy.spatial.distance as ssd
 import numpy as np
 
 
@@ -34,6 +36,65 @@ x = np.genfromtxt(inputpath, delimiter=',')
 y = np.genfromtxt(outputpath, delimiter=',')
 z = np.genfromtxt(coordinatespath, delimiter=',')
 
-print x
-print y
-print z
+
+def calculateCIndex(predictions,index,labels):
+    n = 0
+    h_sum = 0
+    for i in range(len(labels)):
+        t = labels[i][index]
+        p = predictions[i][index]
+        for j in range(i+1,len(labels)):
+            nt = labels[j][index]
+            np = predictions[j][index]
+            if t != nt:
+                n = n + 1
+                if (p < np and t < nt) or (p > np and t > nt):
+                    h_sum = h_sum + 1
+                elif (p < np and t > nt) or (p > np and t < nt):
+                    h_sum = h_sum + 0
+                elif (p == np):
+                    h_sum = h_sum + 0.5
+                    
+    if n == 0:
+        return 0
+    else:
+        return h_sum/n
+
+def LooCV(k):
+    yPredictions = []
+    stdX = calculateZScore()
+    for i in range(len(stdX)):
+        neighbors = inferNeighbors(stdX,stdX[i],y,k,1)
+        yPredictions.append(chooseMajorityLabel(neighbors,k))
+        
+    cIndex = calculateCIndex(yPredictions,0,y)
+    print "Leave-one-out cross-validation"
+    printCIndexes(cIndex)
+
+def chooseMajorityLabel(neighbors,k):
+    predictedOutcome = []
+    for i in range(3):
+        sumOfMod = 0.0
+        for j in range(len(neighbors)):
+            sumOfMod = sumOfMod + neighbors[j][1][i]
+        predictedOutcome.append(sumOfMod/k)
+    
+    return predictedOutcome
+
+
+def inferNeighbors(trainSet,testInstance,labels,k,leaveOut):   
+    distances = []
+    for x in range(len(trainSet)):
+        distances.append((ssd.euclidean(trainSet[x], testInstance), labels[x]))
+        
+    distances.sort(key=operator.itemgetter(0))    
+    return distances[leaveOut:k+leaveOut]
+
+def printCIndexes(cIndex):
+    print 'C Index: {a}'.format(a=cIndex)
+    print
+     
+def calculateZScore():
+    xArr = np.asarray(x)
+    zScores = (xArr - xArr.mean()) / xArr.std() 
+    return zScores
